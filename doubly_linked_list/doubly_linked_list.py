8abc20e317e95ac30c0bc622b1cb5b65ab3c0347
spartners <mariano.scandizzo@spartners.me>
2018-07-17 14:00:44 -0400

    version1

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -19,13 +19,27 @@ class DoublyLinkedList:
     self.tail = None
 
   def add_to_head(self, value):
-    pass
+    new_node  = ListNode(value, None,None)
+    if self.head is None:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      new_node.next = self.head
+      self.head.prev = new_node
+      self.head = new_node
 
   def remove_from_head(self):
     pass
 
   def add_to_tail(self, value):
-    pass
+    new_node  = ListNode(value, None,None)
+    if self.head is None:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      new_node.prev = self.tail
+      self.tail.next = new_node
+      self.tail = new_node
 
   def remove_from_tail(self):
     pass
